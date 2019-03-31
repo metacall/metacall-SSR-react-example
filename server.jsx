673915f97b470b27a1b7589ed4b3a6798f700dee
Unknown <jakash1997@gmail.com>
2019-03-31 22:05:56 +0530

    Update

--- server.jsx
+++ server.jsx
@@ -1,5 +1,6 @@
 const ReactDOMServer = require('react-dom/server');
+const React = require('react')
 
 const element = <h1>Hello</h1>
 
-ReactDOMServer.renderToString(element)
+const myFunc = element => console.log(ReactDOMServer.renderToString(element))
