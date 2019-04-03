15f64ba4bcfe00f21e7d5e21b8b6f008416bd91a
Jose Antonio Dominguez <chamot11@gmail.com>
2019-04-03 09:47:48 -0300

    Returning element rendered string

--- server.jsx
+++ server.jsx
@@ -1,6 +1,6 @@
 const ReactDOMServer = require('react-dom/server');
-const React = require('react')
+const React = require('react');
 
-const element = <h1>Hello</h1>
+const element = <h1>Hello</h1>;
 
-const myFunc = element => console.log(ReactDOMServer.renderToString(element))
+const myFunc = () => ReactDOMServer.renderToString(element);
